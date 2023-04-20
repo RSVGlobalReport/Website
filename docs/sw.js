68cfdea15fe0b811fef7cdce96e90c8fcd5a4a5e
DrGrimshaw <swsbirmingham@gmail.com>
2023-04-20 20:48:17 +0000

    Update gh-pages-testing to output generated at 0e74e04

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/dashboard/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230420.194327';
+const cacheName = 'chirpy-20230420.204108';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
