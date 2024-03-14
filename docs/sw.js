4d39fcceb37e901f4f21474f63a27276b557f45a
deusthindwa <deus.thindwa@gmail.com>
2024-03-14 18:01:35 +0000

    Update gh-pages to output generated at 15bbe53

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/dashboard/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240314.165908';
+const cacheName = 'chirpy-20240314.180123';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
